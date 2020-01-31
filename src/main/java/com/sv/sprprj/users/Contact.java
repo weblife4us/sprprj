720b33a41ed4e8a3a6aa98c4d31afd4755c6f121
weblife4us <ZaQ_3501281270>
2020-01-31 15:19:41 -0500

    Contact added

---
+++ src/main/java/com/sv/sprprj/users/Contact.java
@@ -0,0 +1,18 @@
+package com.sv.sprprj.users;
+
+public class Contact {
+    String name;
+    String surename;
+    String dob;
+    String address;
+    String phone;
+
+    public Contact(String address) {
+        this.address = address;
+        String house;
+        String street;
+        String country;
+        String postcode;
+
+    }
+}
